acb9f2554169210b115015bfedcdb739f842a234
jeevi3686 <jeevitesh3686@gmail.com>
2026-09-01 22:55:33 +0530

    Time: 35 ms (42.41%), Space: 59.1 MB (74.58%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,37 +1,48 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
         
-
         for(int i=0;i<n-2;i++){
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-            if(i>0  && nums[i]==nums[i-1]) continue;
-            int low = i+1;
-            int high = n-1;
+            int left = i+1;
+            int right = n-1;
+            int find = -1*nums[i];
             
+            while(left<right){
+                int sum = nums[left]+nums[right];
+                
+                if(sum==find){
+                    list.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left++;
+                    right--;
+                    // Skip duplicate left values
+                    while (left < right &&
+                           nums[left] == nums[left - 1]) {
+                        left++;
+                    }
 
-            while(low<high){
-                int sum = nums[low]+nums[high] + nums[i];
-
-                if(sum == 0){
-                    res.add(Arrays.asList(nums[i],nums[low],nums[high]));
-                    low++;
-                    high--;
-
-                    while(low<high && nums[low]==nums[low-1]) low++;
-                    while(low<high && nums[high]==nums[high+1]) high--;
+                    // Skip duplicate right values
+                    while (left < right &&
+                           nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                    
+                    
                 }
-
-                else if(sum > 0){
-                    high--;
+                else if(sum>find){
+                    right--;
                 }
                 else{
-                    low++;
+                    left++;
                 }
             }
+            
         }
-        return res;
+        return list;
     }
 }
